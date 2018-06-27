e6214de93dbfa9dd592fae8332ada5a0ce99609c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-06-27 15:32:31 -0400

    handle promises and errors

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -7,9 +7,40 @@ const repoRoot = path.join(__dirname, '..', '..')
 
 // function should return boolean
 // or resolve with a boolean
+// if rejecting, will also retry
+const findRecordAsync = title => {
+  return new Promise((resolve, reject) => {
+    const dbFilename = path.join(repoRoot, 'data.json')
+    fs.readFile(dbFilename, 'utf8', (err, data) => {
+      if (err) {
+        return reject(err)
+      }
+
+      const contents = JSON.parse(data)
+      const todos = contents.todos
+      resolve(todos.find(record => record.title === title))
+    })
+  })
+}
+
+// sync, throwing error if not found
+const findRecordSyncAndThrow = title => {
+  const dbFilename = path.join(repoRoot, 'data.json')
+  const data = fs.readFileSync(dbFilename, 'utf8')
+  const contents = JSON.parse(data)
+  const todos = contents.todos
+  if (todos.find(record => record.title === title)) {
+    return true
+  } else {
+    throw new Error('not found')
+  }
+}
+
+// sync, returning boolean
 const findRecord = title => {
   const dbFilename = path.join(repoRoot, 'data.json')
-  const contents = JSON.parse(fs.readFileSync(dbFilename, 'utf8'))
+  const data = fs.readFileSync(dbFilename, 'utf8')
+  const contents = JSON.parse(data)
   const todos = contents.todos
   return todos.find(record => record.title === title)
 }
@@ -19,17 +50,57 @@ const retryFn = ({ fn, onTimedOut, timeLimitMs = 5000, delay = 50 }) => {
     if (timeLimitMs < 0) {
       return reject(onTimedOut())
     }
-    const found = fn()
-    if (found) {
-      return resolve(true)
-    }
-    setTimeout(() => {
-      retryFn({ fn, onTimedOut, timeLimitMs: timeLimitMs - delay, delay }).then(
-        resolve,
-        reject
+
+    return Promise.try(() => {
+      return fn()
+    })
+      .timeout(timeLimitMs)
+      .catch(Promise.TimeoutError, () => {
+        throw onTimedOut()
+      })
+      .then(
+        found => {
+          if (found) {
+            return resolve(true)
+          }
+
+          // retry after a delay
+          setTimeout(() => {
+            retryFn({
+              fn,
+              onTimedOut,
+              timeLimitMs: timeLimitMs - delay,
+              delay
+            }).then(resolve, reject)
+          }, delay)
+        },
+        () => {
+          // retry after a delay
+          setTimeout(() => {
+            retryFn({
+              fn,
+              onTimedOut,
+              timeLimitMs: timeLimitMs - delay,
+              delay
+            }).then(resolve, reject)
+          }, delay)
+        }
       )
-    }, 50)
   })
+}
+
+const cliRetry = ({ fn, text, timeLimitMs = 5000, delay = 50 }) => {
+  const spinner = ora(text.start).start()
+
+  const onTimedOut = () => new Error(text.fail)
+
+  return retryFn({ fn, onTimedOut, timeLimitMs, delay: 50 })
+    .tap(() => {
+      spinner.succeed(text.pass)
+    })
+    .tapCatch(err => {
+      spinner.fail(err.message)
+    })
 }
 
 module.exports = (on, config) => {
@@ -37,21 +108,16 @@ module.exports = (on, config) => {
   // and doing anything you might want. For example, checking "data.json" file!
   on('task', {
     hasSavedRecord (title, timeLimitMs = 5000) {
-      const spinner = ora(
-        `looking for title "${title}" in the database`
-      ).start()
-
-      const fn = findRecord.bind(null, title)
-      const onTimedOut = () =>
-        new Error(`Could not find record with title "${title}"`)
-
-      return retryFn({ fn, onTimedOut, timeLimitMs, delay: 50 })
-        .tap(() => {
-          spinner.succeed(`found "${title}" in the database`)
-        })
-        .tapCatch(err => {
-          spinner.fail(err.message)
-        })
+      return cliRetry({
+        fn: findRecord.bind(null, title),
+        text: {
+          start: `looking for title "${title}" in the database`,
+          pass: `found "${title}" in the database`,
+          fail: `Could not find record with title "${title}"`
+        },
+        timeLimitMs,
+        delay: 50
+      })
     }
   })
 }
